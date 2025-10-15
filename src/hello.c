04b96d06b6a876c2624651d7d23ca084fb34ece0
Pranav Sivaraman <pranavsivaraman@gmail.com>
2025-10-15 18:22:04 -0400

    make a change only to c code

--- src/hello.c
+++ src/hello.c
@@ -2,7 +2,8 @@
 
 int main() {
   // The main function is the entry point of the program
-  printf("Hello, World!\n"); // Print the string "Hello, World!" followed by a
-                             // newline character
-  return 0;                  // Indicate successful program execution
+  printf("Hello, World!\n");  // Print the string "Hello, World!" followed by a
+  printf("Goodbye World!\n"); // Print the string "Hello, World!" followed by a
+                              // newline character
+  return 0;                   // Indicate successful program execution
 }
