fa2517cb16355c3daa9ad8fcd07845abeb34e620
Pranav Sivaraman <pranavsivaraman@gmail.com>
2025-10-15 17:43:39 -0400

    ci-demo/ci.yaml: add some source code

---
+++ src/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h> // Include the standard input/output library
+
+int main() {
+  // The main function is the entry point of the program
+  printf("Hello, World!\n"); // Print the string "Hello, World!" followed by a
+                             // newline character
+  return 0;                  // Indicate successful program execution
+}
